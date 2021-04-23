a2aad8e0a662a0270ab44f5bf395e882a2a41bf9
Kayofpimentel <kayofpimentel@gmail.com>
2021-04-23 13:27:22 -0300

    Creation of logic and tests for getting and adding balance

--- finance/main.py
+++ finance/main.py
@@ -1,14 +1,42 @@
-def add(stocks, stock):
+def add(wallet, stock):
     stock_name = stock.pop('name')
-    stocks[stock_name] = stock
+    wallet[stock_name] = stock
 
 
-def remove(stocks, stock):
-    return stocks.pop(stock)
+def remove(wallet, stock):
+    return wallet.pop(stock)
 
 
-def total(stocks):
+def total(wallet):
     totalValue = 0
-    for stock in stocks.values():
-        totalValue += stock['value'] * stock['number']
+    for stock in wallet:
+        if 'balance' not in stock:
+            totalValue += wallet[stock]['value'] * wallet[stock]['number']
     return totalValue
+
+
+def show_stocks(wallet):
+    stocks_string = ''
+    for stock in wallet.keys():
+        if stock != 'balance':
+            stock_value = wallet[stock]["value"]
+            stock_number = wallet[stock]["number"]
+            stock_total = stock_value * stock_number
+            stocks_string += f'{stock}: value: {stock_value} number: {stock_number} total: {stock_total}\n'
+    stocks_string += f'Total Wallet: {total(wallet)}'
+    stocks_string += f'Balance: {wallet["balance"]}'
+    return stocks_string
+
+
+def add_balance(wallet, value):
+    balance = wallet['balance']
+    if not isinstance(value, int):
+        return balance
+    elif value < 0:
+        return balance
+    wallet['balance'] += value
+    return wallet['balance']
+
+
+def get_balance(wallet):
+    return wallet['balance']
--- tests/test_finance.py
+++ tests/test_finance.py
@@ -5,22 +5,58 @@ from finance import main
 class TestStockManagement(unittest.TestCase):
 
     def setUp(self):
-        self.stocks = {'stock1': {'value': 150, 'number': 3}}
+        self.wallet = {'stock1': {'value': 150, 'number': 3}, 'balance': 0}
+        self.stock2 = {'name': 'stock2', 'value': 100, 'number': 3}
 
     def test_total_stock_value(self):
-        self.assertEqual(main.total(self.stocks), 450)
+        self.assertEqual(main.total(self.wallet), 450)
 
     def test_add_stock(self):
-        stock2 = {'name': 'stock2', 'value': 100, 'number': 3}
-        main.add(self.stocks, stock2)
-        self.assertEqual(self.stocks['stock2'], {'value': 100, 'number': 3})
-        self.assertEqual(main.total(self.stocks), 750)
+        main.add(self.wallet, self.stock2)
+        self.assertEqual(self.wallet['stock2'], {'value': 100, 'number': 3})
+        self.assertEqual(main.total(self.wallet), 750)
 
     def test_remove_stock(self):
         stock = 'stock1'
-        main.remove(self.stocks, stock)
-        self.assertNotIn(stock, self.stocks)
-        self.assertEqual(main.total(self.stocks), 0)
+        main.remove(self.wallet, stock)
+        self.assertNotIn(stock, self.wallet)
+        self.assertEqual(main.total(self.wallet), 0)
+
+    def test_show_stocks(self):
+        stocks_string = main.show_stocks(self.wallet)
+        self.assertIn('stock1', stocks_string)
+        self.assertIn('value', stocks_string)
+        self.assertIn('number', stocks_string)
+
+        main.add(self.wallet, self.stock2)
+        stocks_string = main.show_stocks(self.wallet)
+        self.assertIn('stock2', stocks_string, f'\n{stocks_string}')
+        self.assertIn('Total Wallet: 750', stocks_string, f'\n{stocks_string}')
+        self.assertIn('Balance', stocks_string, f'\n{stocks_string}')
+
+    def test_get_balance(self):
+        balance = main.get_balance(self.wallet)
+        self.assertEqual(balance, self.wallet['balance'])
+
+    def test_add_balance(self):
+        balance = main.add_balance(self.wallet, 0)
+        self.assertEqual(balance, self.wallet['balance'])
+
+        balance = main.add_balance(self.wallet, 350)
+        self.assertNotEqual(balance, 0)
+        self.assertEqual(balance, self.wallet['balance'])
+
+        balance = main.add_balance(self.wallet, 500)
+        self.assertEqual(balance, self.wallet['balance'])
+
+        main.add_balance(self.wallet, -1000)
+        self.assertEqual(850, self.wallet['balance'])
+
+        main.add_balance(self.wallet, '100')
+        self.assertEqual(850, self.wallet['balance'])
+
+        main.add_balance(self.wallet, 'abc')
+        self.assertEqual(850, self.wallet['balance'])
 
 
 if __name__ == '__main__':
